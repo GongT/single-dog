4fec09e5038a00923ac4675ecd6015c09f3f3fc2
GongT <admin@gongt.me>
2019-07-02 20:24:12 +0800

    update version

--- lib/constants.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const prodPackages: string[];
-export declare const devPackages: string[];
-//# sourceMappingURL=constants.d.ts.map
--- lib/gitName.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const gitUsername: string;
-export declare const gitName: string;
-//# sourceMappingURL=gitName.d.ts.map
